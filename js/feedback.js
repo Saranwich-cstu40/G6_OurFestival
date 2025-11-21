3e7117f2570a9670f908f15c6b67765dea2c3be8
Saranwich <saranwichpochai.idea@gmail.com>
2025-11-21 16:10:17 +0700

    ref(feedback): form validation

---
+++ js/feedback.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const inputs = form.querySelectorAll('input, textarea');
+
+    inputs.forEach(input => {
+        let timer;
+
+        input.addEventListener('input', () => {
+            clearTimeout(timer);
+            input.classList.remove('is-invalid', 'is-valid');
+            
+            timer = setTimeout(() => {
+                if (input.value.trim() !== "") validateInput(input);
+            }, 1000);
+        });
+
+        input.addEventListener('blur', () => {
+            if (input.value.trim() !== "") validateInput(input);
+        });
+    });
+
+    function validateInput(input) {
+        if (input.checkValidity()) {
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+        } else {
+            input.classList.remove('is-valid');
+            input.classList.add('is-invalid');
+        }
+    }
+
+    form.addEventListener('submit', function (e) {
+        if (!form.checkValidity()) {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            form.classList.add('was-validated');
+            
+            inputs.forEach(input => input.classList.remove('is-invalid', 'is-valid'));
+        }
+    });
+});
