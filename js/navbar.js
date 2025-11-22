4a54e5996aa61fdc88eafd4e93bd1319a8c9548c
Tritippayanipa Choochert <tritipchoochert@gmail.com>
2025-11-23 05:34:08 +0700

    feat(navbar): create responsive navbar

---
+++ js/navbar.js
@@ -0,0 +1,50 @@
+function toggleMenu() {
+    document.querySelector("nav").classList.toggle("active");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("nav > ul > li").forEach(li => {
+        li.addEventListener("click", function(e) {
+            if (window.innerWidth <= 768) {
+
+                if (e.target.tagName === "A") {
+                    return;
+                }
+
+                e.preventDefault();
+                e.stopPropagation();
+                li.classList.toggle("open");
+            }
+        });
+    });
+});
+
+document.querySelectorAll("nav ul > li").forEach(item => {
+  const link = item.querySelector(":scope > a");
+  const dropdown = item.querySelector(":scope > ul");
+
+  if (dropdown) {
+    let opened = false;
+
+    link.addEventListener("click", (e) => {
+      if (window.innerWidth <= 768) {
+        if (!opened) {
+          e.preventDefault();
+          item.classList.toggle("open");
+          opened = true;
+
+          document.addEventListener("click", function closeMenu(ev) {
+            if (!item.contains(ev.target)) {
+              item.classList.remove("open");
+              opened = false;
+              document.removeEventListener("click", closeMenu);
+            }
+          });
+
+        } else {
+          opened = false;
+        }
+      }
+    });
+  }
+});
